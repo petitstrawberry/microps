f0d028a495c2866d58fd5ee592a6b7674d04b43f
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-07-12 03:33:45 +0000

    impl: implement ip_input function and integrate ip_init into net_init

--- net.c
+++ net.c
@@ -204,6 +204,11 @@ int net_init(void) {
         return -1;
     }
 
+    if (ip_init() == -1) {
+        errorf("ip_init() failure");
+        return -1;
+    }
+
     infof("initialized");
     return 0;
 }
