eae828d0d0a255ab6215301d748d894269b8ca18
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-07-12 03:08:46 +0000

    impl: loopback transmit

--- driver/loopback.c
+++ driver/loopback.c
@@ -27,6 +27,36 @@ struct loopback_queue_entry {
 
 static int loopback_transmit(struct net_device *dev, uint16_t type,
                              const uint8_t *data, size_t len, const void *dst) {
+    struct loopback_queue_entry *entry;
+    unsigned int num;
+
+    mutex_lock(&PRIV(dev)->mutex);
+    if (PRIV(dev)->queue.num >= LOOPBACK_QUEUE_LIMIT) {
+        mutex_unlock(&PRIV(dev)->mutex);
+        errorf("queue is full");
+        return -1;
+    }
+    
+    entry = memory_alloc(sizeof(*entry) + len);
+    if (!entry) {
+        mutex_unlock(&PRIV(dev)->mutex);
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+    entry->type = type;
+    entry->len = len;
+    memcpy(entry->data, data, len);
+    
+    queue_push(&PRIV(dev)->queue, entry);
+    num = PRIV(dev)->queue.num;
+    mutex_unlock(&PRIV(dev)->mutex);
+    
+    debugf("queue pushed (num:%u), dev=%s, type=0x%04x, len=%zd", num,
+           dev->name, type, len);
+    debugdump(data, len);
+    
+    intr_raise_irq(PRIV(dev)->irq);
+    return 0;
 }
 
 static int loopback_isr(unsigned int irq, void *id) {}
@@ -53,7 +83,7 @@ struct net_device *loopback_init(void) {
     lo = memory_alloc(sizeof(*lo));
     if (!lo) {
         errorf("memory_alloc() failure");
-        memory_free(dev); // Fix memory leak
+        memory_free(dev);  // Fix memory leak
         return NULL;
     }
     lo->irq = LOOPBACK_IRQ;
@@ -63,16 +93,17 @@ struct net_device *loopback_init(void) {
 
     if (net_device_register(dev) == -1) {
         errorf("net_device_register() failure");
-        memory_free(lo); // Fix memory leak
-        memory_free(dev); // Fix memory leak
+        memory_free(lo);   // Fix memory leak
+        memory_free(dev);  // Fix memory leak
         return NULL;
     }
 
     // Register the interrupt handler for loopback device
-    if (intr_request_irq(lo->irq, loopback_isr, INTR_IRQ_SHARED, dev->name, dev) == -1) {
+    if (intr_request_irq(lo->irq, loopback_isr, INTR_IRQ_SHARED, dev->name,
+                         dev) == -1) {
         errorf("intr_request_irq() failure");
-        memory_free(lo); // Fix memory leak
-        memory_free(dev); // Fix memory leak
+        memory_free(lo);   // Fix memory leak
+        memory_free(dev);  // Fix memory leak
         return NULL;
     }
 
