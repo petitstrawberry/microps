1f0dddff294d765cebbd0638e7b228b8a35676c7
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-07-20 14:50:07 +0000

    init: step 10

--- icmp.h
+++ icmp.h
@@ -1,6 +1,38 @@
 #ifndef ICMP_H
 #define ICMP_H
 
+#define ICMP_HDR_SIZE 8
+
+#define ICMP_TYPE_ECHOREPLY           0
+#define ICMP_TYPE_DEST_UNREACH        3
+#define ICMP_TYPE_SOURCE_QUENCH       4
+#define ICMP_TYPE_REDIRECT            5
+#define ICMP_TYPE_ECHO                8
+#define ICMP_TYPE_TIME_EXCEEDED      11
+#define ICMP_TYPE_PARAM_PROBLEM      12
+#define ICMP_TYPE_TIMESTAMP          13
+#define ICMP_TYPE_TIMESTAMPREPLY     14
+#define ICMP_TYPE_INFO_REQUEST       15
+#define ICMP_TYPE_INFO_REPLY         16
+
+
 extern int icmp_init(void);
+
+struct icmp_hdr {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint32_t values;
+};
+
+struct icmp_echo {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint16_t id;
+    uint16_t seq;
+};
+
+static void icmp_dump(const uint8_t *data, size_t len) {}
 
 #endif
