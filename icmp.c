d674945462a57415923cc234c89ecfd7450e9c04
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-07-20 15:37:20 +0000

    init: step11

--- icmp.c
+++ icmp.c
@@ -2,9 +2,12 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "ip.h"
 #include "util.h"
+
+#define ICMP_BUFSIZ IP_PAYLOAD_SIZE_MAX
 
 struct icmp_hdr {
     uint8_t type;
@@ -96,6 +99,10 @@ void icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst,
     debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)),
            ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
     icmp_dump(data, len);
+}
+
+int icmp_output(uint8_t type, uint8_t code, uint32_t values,
+                const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst) {
 }
 
 int icmp_init(void) {
